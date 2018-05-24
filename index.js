56d8a5d7ccda694ed7b45c6410c8927cf1c9ed80
Mobasher Sobhan <moba2k@yahoo.com>
2018-05-24 01:22:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,8 @@ function myAnimal() {
 }  //I can't hard-code 'cat' inside, but, apparently, I can switch yourAnimal and myAnimal function names to get the expected result
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {
